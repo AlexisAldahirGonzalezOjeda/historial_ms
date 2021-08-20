c5d4e6362e6bc1c6e62f74139487663ba72bf516
AlexisAldahirGonzalezOjeda <alexisgonzalez120021@gmail.com>
2021-08-19 22:33:25 -0500

    Arreglo
    Arreglo para utilizar el método POST

--- src/main/java/com/proyecto/historial_ms/controllers/RegistroController.java
+++ src/main/java/com/proyecto/historial_ms/controllers/RegistroController.java
@@ -2,21 +2,28 @@ package com.proyecto.historial_ms.controllers;
 
 import com.proyecto.historial_ms.exceptions.RegistroNotFoundException;
 import com.proyecto.historial_ms.models.Registro;
+import com.proyecto.historial_ms.models.Historial;
 import com.proyecto.historial_ms.repositories.RegistroRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.proyecto.historial_ms.repositories.HistorialRepository;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class RegistroController {
 
     private final RegistroRepository registroRepository;
+    private final HistorialRepository historialRepository;
 
 
-    public RegistroController(RegistroRepository registroRepository) {
+    public RegistroController(RegistroRepository registroRepository, HistorialRepository historialRepository) {
         this.registroRepository = registroRepository;
+        this.historialRepository = historialRepository;
 
         //Seed
         Registro newRegistro01 = new Registro("123456789", "Juan", "Perez", new Date(91, 4,19), "Masculino", "Soltero", "Publico", "Independiente", "Calle 10, carrera 9", "3011234567", "0", "JuanPerez@mai.com");
@@ -30,5 +37,16 @@ public class RegistroController {
     Registro getRegistro(@PathVariable String userId){
         return registroRepository.findById(userId)
                 .orElseThrow(() -> new RegistroNotFoundException("No se encontro un registro con el userId: " + userId));
+    }
+
+    @PostMapping("/registrar")
+    Historial newRegistro(@RequestBody Historial historiales){
+        Registro history = registroRepository.findById(historiales.getId()).orElse(null);
+        if (history == null){
+            throw new RegistroNotFoundException("No se encontró una cuenta con el user Id:" + historiales.getId());
+        } else{
+            return historialRepository.save(historiales);
+        }
+        
     }
 }
